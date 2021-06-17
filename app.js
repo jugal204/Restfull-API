261c5f0a331cbd40d78b78452a730f565dfa21c6
jugal204 <62797131+jugal204@users.noreply.github.com>
2021-06-17 18:58:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(express.static("public"));
+app.use(express.static("public"));// static folder
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
 
 mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
 
@@ -124,6 +125,6 @@ app.route("/articles/:articleTitle")
 
 
 
-app.listen(3000, function() {
+app.listen(process.env.Port||3000, function() {
   console.log("Server started on port 3000");
 });
